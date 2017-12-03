4c676b46a249ff542b68299124489ea1c9add509
Adrian Akison <adrian@akison.com>
2017-12-03 12:13:27 +1000

    Add WaitFor from other repo, add .meta files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # behaviour-salsa-unity
 Behaviours, helpers, utilities that help with Unity development.
+
+## Installation
+This repo is designed to be used as a submodule of an existing Unity3D repo.  E.g.
+
+```
+    git submodule add https://github.com/aakison/behaviour-salsa-unity.git Assets/Modules/BehaviourSalsa
+```
+
+There are no demo scenes or resources in this repo, just the required files so that your solution is not polluted with unnecessary files.
+
+## Feature: WaitFor helper
+
+In Unity Coroutines, creating new objects in yield statements causes unnecessary heap allocations.  Use `WaitFor.Seconds(s)` to use a cached version.
+
---
+++ WaitFor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class WaitFor {
+
+    public static WaitForSeconds Seconds(float seconds) {
+        WaitForSeconds ret;
+        if(!cachedWaiters.TryGetValue(seconds, out ret)) {
+            ret = new WaitForSeconds(seconds);
+            cachedWaiters.Add(seconds, ret);
+        }
+        return ret;
+    }
+
+    private static Dictionary<float, WaitForSeconds> cachedWaiters = new Dictionary<float, WaitForSeconds>(new FloatComparer());
+
+    public const object NextFrame = null;
+
+    public static readonly object EndOfFrame = new WaitForEndOfFrame();
+
+    private class FloatComparer : IEqualityComparer<float> {
+        public bool Equals(float x, float y) {
+            return x == y;
+        }
+
+        public int GetHashCode(float obj) {
+            return obj.GetHashCode();
+        }
+    }
+
+}
