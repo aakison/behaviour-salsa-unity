1918cd9f54ea43dd9bcd8ef13d7d915b5b7f4a92
Adrian Akison <adrian@akison.com>
2018-05-20 16:16:32 +1000

    Add destroy on timeout

---
+++ DestroyOnTimeout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Salsa {
+
+    public class DestroyOnTimeout : MonoBehaviour {
+
+        [Tooltip("The number of seconds to wait before the gameObject is automatically destroyed.")]
+        public float timeout;
+
+        private IEnumerator Start() {
+            yield return WaitFor.Seconds(timeout);
+            Destroy(gameObject);
+        }
+
+    }
+}
