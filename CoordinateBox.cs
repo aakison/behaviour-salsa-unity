06734e218cf63abaca3ce604220cc0624fb7769c
Adrian Akison <adrian@akison.com>
2018-09-16 20:39:44 +1000

    Remove relentless namespace

--- CoordinateBox.cs
+++ CoordinateBox.cs
@@ -9,94 +9,101 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-namespace Relentless {
+/// <summary>
+/// Represents a bounding box of all coordinates from [minimum, maximum).
+/// </summary>
+public struct CoordinateBox {
+
+    public Coordinate minimum;
+
+    public Coordinate maximum;
 
     /// <summary>
-    /// Represents a bounding box of all coordinates from [minimum, maximum).
+    /// Construct a new CoordinateBox over the space [min, max).
     /// </summary>
-    public struct CoordinateBox {
+    /// <param name="min">The inclusive lower bound of the coordinate box.</param>
+    /// <param name="max">The exclusive upper bound of the coordinate box.</param>
+    public CoordinateBox(Coordinate min, Coordinate max)
+    {
+        minimum = min;
+        maximum = max;
+    }
 
-        public Coordinate minimum;
-
-        public Coordinate maximum;
-
-        /// <summary>
-        /// Construct a new CoordinateBox over the space [min, max).
-        /// </summary>
-        /// <param name="min">The inclusive lower bound of the coordinate box.</param>
-        /// <param name="max">The exclusive upper bound of the coordinate box.</param>
-        public CoordinateBox(Coordinate min, Coordinate max) {
-            minimum = min;
-            maximum = max;
-        }
-
-        public Coordinate Dimensions {
-            get {
-                return maximum - minimum;
-            }
-        }
-
-        public void ForEach(Action<Coordinate> action) {
-            if(action == null) {
-                throw new ArgumentNullException("action");
-            }
-            for(var x = minimum.x; x < maximum.x; ++x) {
-                for(var y = minimum.y; y < maximum.y; ++y) {
-                    for(var z = minimum.z; z < maximum.z; ++z) {
-                        action(new Coordinate(x, y, z));
-                    }
-                }
-            }
-        }
-
-        public bool Contains(Coordinate coordinate) {
-            // TODO: Optimize here with short circuit evaluation if necessary.
-            var containsX = minimum.x <= coordinate.x && coordinate.x < maximum.x;
-            var containsY = minimum.y <= coordinate.y && coordinate.y < maximum.y;
-            var containsZ = minimum.z <= coordinate.z && coordinate.z < maximum.z;
-            return containsX && containsY && containsZ;
-        }
-
-        /// <summary>
-        /// Checks equality with a pair-wise evaluation of components.
-        /// </summary>
-        public static bool operator ==(CoordinateBox lhs, CoordinateBox rhs) {
-            return lhs.minimum == rhs.minimum && lhs.maximum == rhs.maximum;
-        }
-
-        /// <summary>
-        /// Checks inequality with a pair-wise evaluation of components.
-        /// </summary>
-        public static bool operator !=(CoordinateBox lhs, CoordinateBox rhs) {
-            return !(lhs == rhs);
-        }
-
-        public override bool Equals(object obj) {
-            if(obj is CoordinateBox) {
-                return this == (CoordinateBox)obj;
-            }
-            else {
-                return false;
-            }
-        }
-
-        public override int GetHashCode() {
-            return minimum.GetHashCode() ^ maximum.GetHashCode();
-        }
-
-        public void ForEachExcept(CoordinateBox exclude, Action<Coordinate> action) {
-            // TODO: Optimize here by unrolling loops and jumping over the large runs of numbers.
-            ForEach(coord => {
-                if(!exclude.Contains(coord)) {
-                    action(coord);
-                }
-            });
-        }
-
-        public override string ToString() {
-            return String.Format("CB[{0}, {1})", minimum, maximum);
+    public Coordinate Dimensions {
+        get {
+            return maximum - minimum;
         }
     }
+
+    public void ForEach(Action<Coordinate> action)
+    {
+        if(action == null) {
+            throw new ArgumentNullException("action");
+        }
+        for(var x = minimum.x; x < maximum.x; ++x) {
+            for(var y = minimum.y; y < maximum.y; ++y) {
+                for(var z = minimum.z; z < maximum.z; ++z) {
+                    action(new Coordinate(x, y, z));
+                }
+            }
+        }
+    }
+
+    public bool Contains(Coordinate coordinate)
+    {
+        // TODO: Optimize here with short circuit evaluation if necessary.
+        var containsX = minimum.x <= coordinate.x && coordinate.x < maximum.x;
+        var containsY = minimum.y <= coordinate.y && coordinate.y < maximum.y;
+        var containsZ = minimum.z <= coordinate.z && coordinate.z < maximum.z;
+        return containsX && containsY && containsZ;
+    }
+
+    /// <summary>
+    /// Checks equality with a pair-wise evaluation of components.
+    /// </summary>
+    public static bool operator ==(CoordinateBox lhs, CoordinateBox rhs)
+    {
+        return lhs.minimum == rhs.minimum && lhs.maximum == rhs.maximum;
+    }
+
+    /// <summary>
+    /// Checks inequality with a pair-wise evaluation of components.
+    /// </summary>
+    public static bool operator !=(CoordinateBox lhs, CoordinateBox rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if(obj is CoordinateBox) {
+            return this == (CoordinateBox)obj;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return minimum.GetHashCode() ^ maximum.GetHashCode();
+    }
+
+    public void ForEachExcept(CoordinateBox exclude, Action<Coordinate> action)
+    {
+        // TODO: Optimize here by unrolling loops and jumping over the large runs of numbers.
+        ForEach(coord => {
+            if(!exclude.Contains(coord)) {
+                action(coord);
+            }
+        });
+    }
+
+    public override string ToString()
+    {
+        return String.Format("CB[{0}, {1})", minimum, maximum);
+    }
+}
 
 
 #if UNITY_EDITOR
@@ -104,7 +111,8 @@ namespace Relentless {
 [CustomPropertyDrawer(typeof(CoordinateBox))]
 public class CoordinateBoxDrawer : PropertyDrawer {
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
 
         EditorGUI.BeginProperty(position, label, property);
 
@@ -128,4 +136,3 @@ public class CoordinateBoxDrawer : PropertyDrawer {
 
 #endif
 
-}
